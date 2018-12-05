de04c52ad6383581e78eb9f17ad2a8ff8005220d
melancholist <adriandaniel3001@gmail.com>
2018-12-06 05:54:29 +0800

    schedule class

---
+++ JustRipeFarm_v3/Schedule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeFarm_v3
+{
+    public class Schedule
+    {
+        private int id;
+        private string Labourer;
+        private int farmSector;
+        private DateTime date;
+
+        public int Id { get => id; set => id = value; }
+        public string Labourer1 { get => Labourer; set => Labourer = value; }
+        public int FarmSector { get => farmSector; set => farmSector = value; }
+        public DateTime Date { get => date; set => date = value; }
+    }
+
+    public class Sowing : Schedule
+    {
+        private string method;
+        private string seed;
+        private decimal quantity;
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public string Method { get => method; set => method = value; }
+        public string Seed { get => seed; set => seed = value; }
+        public decimal Quantity { get => quantity; set => quantity = value; }
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime EndTime { get => endTime; set => endTime = value; }
+    }
+
+    public class Harvest : Schedule
+    {
+        private string method;
+        private int storageUnit;
+        private string plantType;
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public string Method { get => method; set => method = value; }
+        public int StorageUnit { get => storageUnit; set => storageUnit = value; }
+        public string PlantType { get => plantType; set => plantType = value; }
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime EndTime { get => endTime; set => endTime = value; }
+    }
+
+    public class Treating : Schedule
+    {
+        private string fertiliserUsed;
+        private string quantity;
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public string FertiliserUsed { get => fertiliserUsed; set => fertiliserUsed = value; }
+        public string Quantity { get => quantity; set => quantity = value; }
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime EndTime { get => endTime; set => endTime = value; }
+    }
+
+    public class Driving : Schedule
+    {
+        private string vehicleUsed;
+
+        public string VehicleUsed { get => vehicleUsed; set => vehicleUsed = value; }
+    }
+}
