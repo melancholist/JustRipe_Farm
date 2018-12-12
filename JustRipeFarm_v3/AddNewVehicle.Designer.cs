b0694eca062f384641637792dd6d7ce0b50ee6e6
Deborah Choo <deborahchoo97@gmail.com>
2018-12-12 10:59:57 +0800

    addNewVehicle

--- JustRipeFarm_v3/AddNewVehicle.Designer.cs
+++ JustRipeFarm_v3/AddNewVehicle.Designer.cs
@@ -29,6 +29,21 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxVehicleType = new System.Windows.Forms.ComboBox();
+            this.comboBoxVehicleStatus = new System.Windows.Forms.ComboBox();
+            this.textBoxModelName = new System.Windows.Forms.TextBox();
+            this.textBoxEngNo = new System.Windows.Forms.TextBox();
+            this.textBoxWeightLimit = new System.Windows.Forms.TextBox();
+            this.btbSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +56,163 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "New Vehicle";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vehicle Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Model name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Production Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Engine Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Weight Limit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 281);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Vehicle Status";
+            // 
+            // comboBoxVehicleType
+            // 
+            this.comboBoxVehicleType.FormattingEnabled = true;
+            this.comboBoxVehicleType.Items.AddRange(new object[] {
+            "Truck",
+            "Chiller truck",
+            "Trailer truck",
+            "Van",
+            "Other"});
+            this.comboBoxVehicleType.Location = new System.Drawing.Point(219, 97);
+            this.comboBoxVehicleType.Name = "comboBoxVehicleType";
+            this.comboBoxVehicleType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxVehicleType.TabIndex = 7;
+            // 
+            // comboBoxVehicleStatus
+            // 
+            this.comboBoxVehicleStatus.FormattingEnabled = true;
+            this.comboBoxVehicleStatus.Items.AddRange(new object[] {
+            "Available",
+            "Unavailable",
+            "In Maintenance"});
+            this.comboBoxVehicleStatus.Location = new System.Drawing.Point(219, 278);
+            this.comboBoxVehicleStatus.Name = "comboBoxVehicleStatus";
+            this.comboBoxVehicleStatus.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxVehicleStatus.TabIndex = 8;
+            // 
+            // textBoxModelName
+            // 
+            this.textBoxModelName.Location = new System.Drawing.Point(219, 135);
+            this.textBoxModelName.Name = "textBoxModelName";
+            this.textBoxModelName.Size = new System.Drawing.Size(120, 22);
+            this.textBoxModelName.TabIndex = 9;
+            // 
+            // textBoxEngNo
+            // 
+            this.textBoxEngNo.Location = new System.Drawing.Point(219, 213);
+            this.textBoxEngNo.Name = "textBoxEngNo";
+            this.textBoxEngNo.Size = new System.Drawing.Size(120, 22);
+            this.textBoxEngNo.TabIndex = 11;
+            // 
+            // textBoxWeightLimit
+            // 
+            this.textBoxWeightLimit.Location = new System.Drawing.Point(219, 241);
+            this.textBoxWeightLimit.Name = "textBoxWeightLimit";
+            this.textBoxWeightLimit.Size = new System.Drawing.Size(121, 22);
+            this.textBoxWeightLimit.TabIndex = 12;
+            // 
+            // btbSave
+            // 
+            this.btbSave.Location = new System.Drawing.Point(264, 361);
+            this.btbSave.Name = "btbSave";
+            this.btbSave.Size = new System.Drawing.Size(75, 23);
+            this.btbSave.TabIndex = 13;
+            this.btbSave.Text = "Save";
+            this.btbSave.UseVisualStyleBackColor = true;
+            this.btbSave.Click += new System.EventHandler(this.btbSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(154, 361);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(357, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "kg";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(219, 176);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
             // AddNewVehicle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.ClientSize = new System.Drawing.Size(465, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btbSave);
+            this.Controls.Add(this.textBoxWeightLimit);
+            this.Controls.Add(this.textBoxEngNo);
+            this.Controls.Add(this.textBoxModelName);
+            this.Controls.Add(this.comboBoxVehicleStatus);
+            this.Controls.Add(this.comboBoxVehicleType);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "AddNewVehicle";
             this.Text = "AddNewVehicle";
@@ -57,5 +224,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxVehicleType;
+        private System.Windows.Forms.ComboBox comboBoxVehicleStatus;
+        private System.Windows.Forms.TextBox textBoxModelName;
+        private System.Windows.Forms.TextBox textBoxEngNo;
+        private System.Windows.Forms.TextBox textBoxWeightLimit;
+        private System.Windows.Forms.Button btbSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
