8f0743901fc20cd327cab598b8ea7df7c0a5d916
melancholist <adriandaniel3001@gmail.com>
2018-12-16 11:10:00 +0800

    unit testing (do as much as you can)

---
+++ JustRipeFarm_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JustRipeFarm_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("JustRipeFarm_UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d35a5a2-8765-4cb9-a11b-88c6d38bbe6b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JustRipeFarm_UnitTest/VehicleHandlerUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using JustRipeFarm_v3;
+
+namespace JustRipeFarm_UnitTest
+{
+    [TestClass]
+    public class VehicleHandlerUnitTest
+    {
+        [TestMethod]
+        public void AddNewVehicleTest()
+        {
+            DbConnector con = new DbConnector();
+            string resp = con.connect();
+            Assert.AreEqual("Done", resp);
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.VehicleType = "Truck";
+            vehicle.ModelName = "starz45";
+            vehicle.ProductionYear = 2015;
+            vehicle.EngineNumber = "56562651";
+            vehicle.WeightLimit = 500.20M;
+            vehicle.VehicleStatus = "Available";
+            vehicle.Price = 1522.00M;
+
+            VehicleHandler vehicHandlr = new VehicleHandler();
+            int resp2 = vehicHandlr.addNewVehicle(con.getConn(), vehicle);
+            Assert.AreEqual(1, resp2);
+        }
+    }
+}
