6763fd16369a19b49f7974bef7a9a338e1b424e8
melancholist <adriandaniel3001@gmail.com>
2018-12-16 13:36:18 +0800

    added 2 more unit test

---
+++ JustRipeFarm_UnitTest/FertiliserHandlerUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using JustRipeFarm_v3;
+
+namespace JustRipeFarm_UnitTest
+{
+    [TestClass]
+    public class FertiliserHandlerUnitTest
+    {
+        [TestMethod]
+        public void AddNewFertiliser()
+        {
+            DbConnector dbconn = new DbConnector();
+            string resp = dbconn.connect();
+            Assert.AreEqual("Done", resp);
+
+            Fertiliser fert = new Fertiliser();
+            fert.FertiliserType = "Urea";
+            fert.FertiliserQuantity = 30.00M;
+            fert.FertiliserCompany = "Just Good Turds";
+
+            FertiliserHandler fertHandlr = new FertiliserHandler();
+            int resp2 = fertHandlr.addNewFertiliser(dbconn.getConn(), fert);
+            Assert.AreEqual(1, resp2);
+        }
+    }
+}
---
+++ JustRipeFarm_UnitTest/MachineHandlerUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using JustRipeFarm_v3;
+
+namespace JustRipeFarm_UnitTest
+{
+    [TestClass]
+    public class MachineHandlerUnitTest
+    {
+        [TestMethod]
+        public void AddNewMachine()
+        {
+            DbConnector conn = new DbConnector();
+            string resp = conn.connect();
+            Assert.AreEqual("Done", resp);
+
+            Machine machine = new Machine();
+            machine.MachineType = "Harvester";
+            machine.ModelName = "turbulance400";
+            machine.MachineStatus = "Available";
+            machine.Price = 1522.00M;
+
+            MachineHandler mechHandlr = new MachineHandler();
+            int resp2 = mechHandlr.addNewMachine(conn.getConn(), machine);
+            Assert.AreEqual(1, resp2);
+        }
+    }
+}
